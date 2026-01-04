9b026f7c5e2d5d70af4d85141f874f90cd5f165a
ISUCKAZZ <168322041+ISUCKAZZ@users.noreply.github.com>
2026-01-04 04:00:29 -0500

    fix

--- script.js
+++ script.js
@@ -185,12 +185,3 @@ window.addEventListener("load", () => {
     });
 
 });
-<script>
-    window.addEventListener('scroll', () => {
-        if (window.scrollY > 100) {
-            document.documentElement.classList.add('scrolled');
-        } else {
-            document.documentElement.classList.remove('scrolled');
-        }
-    });
-</script>
